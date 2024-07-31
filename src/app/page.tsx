dcc4fc80027179c8a4e7397c471cb235c854ccfe
lohputer <leynalyndon@gmail.com>
2024-07-31 17:26:07 +0800

    505

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main className="bg-orange-500 flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="border-yellow-500 border-4 z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex lg:flex-col ">
+        <h1 className="text-2xl text-yellow-400 font-semibold">lyndon loh jing yu 罗劲宇</h1>
+      </div>
+    </main>
+  );
+}
