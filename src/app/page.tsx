ccb0399e91f373ccf8a00116bc13fcf91eaa0f15
lohputer <leynalyndon@gmail.com>
2024-08-02 21:09:04 +0800

    testing responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 
 export default function Home() {
   return (
-    <main className="bg-orange-500 flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex lg:flex-col ">
-        <nav className="border border-4 z-10 w-full max-w-5xl items-left justify-left gap-5 font-mono text-sm flex flex-wrap">
-          <a className="text-yellow-400" href="https://github.com/lohputer">GitHub</a>
-          <a className="text-yellow-400" href="">Idk</a>
-        </nav>
-        <p className="text-center text-yellow-400">Hi I'm</p>
-        <h1 className="text-center text-2xl text-yellow-400 font-semibold">lohputer</h1>
+    <main className="bg-orange-500 flex min-h-screen flex-col items-centers">
+      <nav className="fixed p-5 z-10 w-full font-mono text-md gap-5 flex flex-row">
+        <a className="text-sm md:text-base lg:text-lg text-yellow-400" href="https://github.com/lohputer">GitHub</a>
+        <a className="float-right text-base lg:text-lg text-yellow-400" href="">Idk</a>
+      </nav>
+      <div className="pt-20 z-10 items-center justify-center font-mono text-md lg:flex lg:flex-col w-full">
+        <p className="text-center text-sm lg:text-xl text-yellow-400">Hi I'm</p>
+        <h1 className="text-center text-2xl lg:text-4xl text-yellow-400 font-semibold">lohputer</h1>
       </div>
     </main>
   );
