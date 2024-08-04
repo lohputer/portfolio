c1f3f5150d89baabe4999ecad87e8a367a8314a0
lohputer <leynalyndon@gmail.com>
2024-08-04 20:30:33 +0800

    Update fetchLanguages.tsx

--- src/app/fetchLanguages.tsx
+++ src/app/fetchLanguages.tsx
@@ -1,12 +1,12 @@
-"use server";
+
 export async function fetchLanguages() {
-    const res = await fetch('coding.txt');
+    const res = await fetch("coding.txt");
     const data = await res.text();
     const words = data.split('END\r\n');
     const fetchedLangs: [string, string][] = words.map((word) => {
-      const paragraph = word.split('\r\n');
-      const string = paragraph[0];
-      return [string, paragraph.slice(1).join('~')];
+        const paragraph = word.split('\r\n');
+        const string = paragraph[0];
+        return [string, paragraph.slice(1).join('~')];
     });
     return fetchedLangs;
-  }
+}
